82cbec2fefa4b29c1fdb6f56a43166bd3ed96c93
takealook <macaroonv@gmail.com>
2024-05-11 16:59:27 +0900

    배너 관리 화면에서 이미지가 정상적으로 나오지 않는 오류 수정

--- src/main/java/com/zerobase/fastlms/admin/controller/AdminBannerController.java
+++ src/main/java/com/zerobase/fastlms/admin/controller/AdminBannerController.java
@@ -15,10 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.UUID;
 
@@ -62,12 +60,12 @@ public class AdminBannerController extends BaseController {
             @RequestParam("bannerImage") MultipartFile file) {
 
         try {
-            String uploadDir = "/Users/ttekkeollug/Desktop/fastlms3_banner";
-            String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+            String uploadDir = "src/main/resources/static/res/se2/img/banner";
+            String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
             Path filePath = Paths.get(uploadDir, fileName);
-            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+            file.transferTo(filePath);
 
-            parameter.setImagePath(filePath.toString());
+            parameter.setImagePath(fileName);
 
             boolean result = bannerService.register(parameter);
             model.addAttribute("result", result);
